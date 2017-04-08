17465d768c009a2eaadfe7b220637b16e2208eb7
thomasonw <al_thomason@iname.com>
2017-04-07 17:08:20 -0700

    updates

--- NMEA2000_avr.cpp
+++ NMEA2000_avr.cpp
@@ -34,6 +34,7 @@ Inherited NMEA2000 object for Atmel AVR UCs with internal CAN
 based setup. See also NMEA2000 library.
 */
 
+
 #include <NMEA2000_avr.h> 
 #include <avr_can.h>                            // https://github.com/thomasonw/avr_can
 
@@ -45,38 +46,31 @@ tNMEA2000_avr::tNMEA2000_avr() : tNMEA2000() {
 //*****************************************************************************
 bool tNMEA2000_avr::CANSendFrame(unsigned long id, unsigned char len, const unsigned char *buf, bool wait_sent) {
   CAN_FRAME output;
-  bool result;
-  int TryCount=0;
+
   
     output.extended=true;
     output.id = id;
     output.length = len;
     for (unsigned char i=0; i<len && i<8; i++) output.data.bytes[i]=buf[i];
     
-    do {
-      if (TryCount>0) delay(3);
-      result=CAN.sendFrame(output);
-      TryCount++;
-    } while (TryCount<2 && !result);
-    
-    return result;
-}
+    return (CAN.sendFrame(output));
+ 
+ }
 
 //*****************************************************************************
 bool tNMEA2000_avr::CANOpen() {
     Can0.begin(CAN_BPS_250K);
-  //By default there are 7 mailboxes for each device that are RX boxes
+  //By default there are 6 mailboxes, 5x of which are used for Rx boxes
   //This sets each mailbox to have an open filter that will accept extended
   //or standard frames
   int filter;
-  //extended
-  for (filter = 0; filter < 5; filter++) {
-  	Can0.setRXFilter(filter, 0, 0, true);
+
+  for (filter = 0; filter < 4; filter++) {
+  	Can0.setRXFilter(filter, 0, 0, true);		// Majority of them for extended messages	
   }  
-  //standard
-  for (int filter = 5; filter < 7; filter++) {
-  	Can0.setRXFilter(filter, 0, 0, false);
-  }  
+
+  Can0.setRXFilter(4, 0, 0, false);			// Use the last one for std messages
+
     return true;
 }
 
